a266e32aaf34e6cf936aad30b222c7ad4547a301
Matt Mason <matthewmason0@gmail.com>
2018-12-06 05:22:25 -0600

    Yield tensors according to descriptions

--- generatorFunction.py
+++ generatorFunction.py
@@ -3,22 +3,28 @@ import sys
 import numpy as np
 import random
 
-np.set_printoptions(threshold=np.nan)
+# np.set_printoptions(threshold=np.nan)
 
 def dataGenerator(dir, batchSize):
-    filename = random.choice(os.listdir(dir))
-    path = os.path.join(dir,filename)
-    r, intensity, label = np.loadtxt(path, delimiter=' ', usecols=(0,2,3), unpack=True)
+    while True:
+        inputTensor = np.empty((0, 1081, 2))
+        outputTensor = np.empty((0, 1081, 2))
+        for j in range(0, batchSize):
+            filename = random.choice(os.listdir(dir)) #random.sample() would pick unique files
+            path = os.path.join(dir, filename)
+            r, intensity, label = np.loadtxt(path, delimiter=' ', usecols=(0,2,3), unpack=True)
 
-    # normalize intensity
-    intensity = (intensity - np.min(intensity)) / np.ptp(intensity)
+            # normalize intensity
+            intensity = (intensity - np.min(intensity)) / np.ptp(intensity)
 
-    print("Chose file {}".format(filename))
-    print("R:")
-    print(r)
-    print("INTENSITY:")
-    print(intensity)
-    print("LABEL:")
-    print(label)
+            obstacle = label.astype(int)
+            notObstacle = 1 - label.astype(int)
 
-dataGenerator("/Users/matthew/Box/Astrobotics Software/ml-od/11-17/labeled", 1)
+            inputTensor = np.append(inputTensor, np.dstack((r, intensity)), axis=0)
+            outputTensor = np.append(outputTensor, np.dstack((obstacle, notObstacle)), axis=0)
+
+        yield inputTensor, outputTensor
+
+for tensor in dataGenerator("/Users/matthew/astrobot/rmc-ml-od/11-17/labeled", 2):
+    print(tensor)
+    break
