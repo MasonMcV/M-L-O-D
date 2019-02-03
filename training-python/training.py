8661399afccefcae8f204a1c70a0ec820050dfab
Mason McVicker <mmcvicker@crimson.ua.edu>
2019-02-02 19:44:47 -0600

    Adding predict stage

--- training-python/training.py
+++ training-python/training.py
@@ -8,7 +8,6 @@ import numpy as np
 import random
 
 
-# np.set_printoptions(threshold=np.nan)
 
 def dataGenerator(directory, batchSize):
     while True:
@@ -38,42 +37,8 @@ def dataGenerator(directory, batchSize):
             inputTensor = np.append(inputTensor, np.dstack((r, intensity)), axis=0)
             outputTensor = np.append(outputTensor, np.dstack((obstacle, notObstacle)), axis=0)
 
-        print(np.shape(inputTensor))
+        #print(np.shape(inputTensor))
         yield inputTensor, outputTensor
-
-
-
-# inputTensor = np.empty((0, 1081, 2))
-# outputTensor = np.empty((0, 1081, 2))
-#
-# filename = random.choice(os.listdir('data/'))  # random.sample() would pick unique files
-# path = os.path.join(dir, filename)
-# r, intensity, label = np.loadtxt(path, delimiter=',', usecols=(0, 2, 3), unpack=True)
-
-# to flip, or not to flip
-# if random.choice((True, False)):
-#     r         = np.flip(r, 0)
-#     intensity = np.flip(intensity, 0)
-#     label     = np.flip(label, 0)
-
-# add gaussian noise
-# r         += np.random.normal(0, 1, r.shape) # median, std dev, size
-# intensity += np.random.normal(0, 1, intensity.shape)
-
-# normalize intensity
-# intensity = (intensity - np.min(intensity)) / np.ptp(intensity)
-#
-# obstacle = label.astype(int)
-# notObstacle = 1 - label.astype(int)
-#
-# inputTensor = np.append(inputTensor, np.dstack((r, intensity)), axis=0)
-# outputTensor = np.append(outputTensor, np.dstack((obstacle, notObstacle)), axis=0)
-
-# Generate dummy data
-# x_train = np.random.random((1000, 20, 1))
-# y_train = np.random.randint(1, size=(1000, 1))
-# x_test = np.random.random((100, 20))
-# y_test = np.random.randint(1, size=(100, 1))
 
 
 model = Sequential()
@@ -86,7 +51,18 @@ model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit_generator(dataGenerator('data/', 13), epochs=20, steps_per_epoch=13)
+model.fit_generator(dataGenerator('data/', 13), epochs=2, steps_per_epoch=13)
+
+
+#model.summary()
+
+test = model.predict_generator(dataGenerator('test/', 1), steps=1)
+
+print model.metrics_names
+
+np.set_printoptions(threshold=sys.maxsize)
+
+print test
 
 # score = model.evaluate(dataGenerator('data/', 5), batch_size=5)
 
